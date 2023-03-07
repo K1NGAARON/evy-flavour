25aa4737120048559e3f39f4b8df8209aab13035
K1NGAARON <gerardaaron1997&gmail.com>
2023-03-07 21:02:37 +0100

    travel page

---
+++ travel/js/script.js
@@ -0,0 +1,45 @@
+const content = [
+    {
+        title: "this is a title",
+        body: "this is some body text",
+        img: "",
+        url: 'this is the url'
+    },
+    {
+        title: "this is a title",
+        body: "this is some body text",
+        img: "",
+        url: 'this is the url'
+    },
+    {
+        title: "this is a title",
+        body: "this is some body text",
+        img: "",
+        url: 'this is the url'
+    }
+];
+
+function createCards(e) {
+    const target = document.querySelector('#travel-wrapper');
+
+    for (let i = 0; i < content.length; i++) {
+        let template = `
+            <div class="card">
+                <img src="${content[i].img}" alt="${content[i].title}">
+                <h5>
+                    ${content[i].title}
+                </h5>
+                <p>
+                    ${content[i].body}
+                </p>
+                <a href="${content[i].url}" class="btn ghost-btn">
+                    Lees meer
+                </a>
+            </div>
+        `
+
+        target.insertAdjacentHTML('beforeend', template);
+    }
+};
+
+$(document).ready(createCards);
