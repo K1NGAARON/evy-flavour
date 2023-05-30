c30dfe08475ab8007ef21dd5a33a1afd5cb78e09
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-30 17:30:35 +0200

    added cookie policy

--- js/app.js
+++ js/app.js
@@ -33,3 +33,27 @@ const targets = document.querySelectorAll(animationTarget);
 for (const target of targets) {
     observer.observe(target);
 }
+
+const cookieBanner = document.querySelector(".cookie-banner-wrapper");
+const cookiesAccepted = document.querySelector("#accept-cookies");
+const blackOverlay = document.querySelector("#overlay");
+
+// Accept cookies
+cookiesAccepted.addEventListener("click", function acceptCookies() {
+    blackOverlay.classList.add("hide-cookie-banner");
+    cookieBanner.classList.add("hide-cookie-banner");
+    storeCookieConsent();
+});
+
+function storeCookieConsent() {
+    localStorage.cookies = "accepted";
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    if ("cookies" in localStorage) {
+        blackOverlay.classList.add("hide-cookie-banner");
+        cookieBanner.classList.add("hide-cookie-banner");
+    } else {
+        // Shows cookieBanner
+    }
+});
